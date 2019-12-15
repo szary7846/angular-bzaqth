d77924c5a83aef6bf96173383d856f079567c0aa
szary7846 <danielszarynski@interia.pl>
2019-12-15 12:59:33 +0100

    4 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Wydatek, KATEGORIE } from './wydatek';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+   wydatki: Wydatek[];
+
+   ngOnInit() {
+    this.wydatki = [
+        new Wydatek('Tankowanie', 252.43, '', new Date(2018, 4, 30),50.5,841),
+        new Wydatek('Myjnia', 75, '', new Date(2018, 4, 2)),
+        new Wydatek('Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16)),
+        new Wydatek('Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16)),
+        new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12),65.555,''),
+        new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12),'',1300)
+    ];
 }
+}
+
