f9cd1488caf23f9f6cba46ab7c262ecbe3bc01bc
szary7846 <danielszarynski@interia.pl>
2019-12-17 10:48:16 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ListaWydatkowComponent } from './lista-wydatkow/lista-wydatkow.component';
 import { ListaWydatkowService } from './lista-wydatkow.service';
 import { DodajWydatekComponent } from './dodaj-wydatek/dodaj-wydatek.component';
 
+const appRoutes: Routes = [
+  { path: 'wydatki', component: ListaWydatkowComponent },
+  { path: 'dodaj', component: DodajWydatekComponent },
+  { path: '',
+    redirectTo: '/wydatki',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes) ],
   declarations: [ AppComponent, ListaWydatkowComponent, DodajWydatekComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ListaWydatkowService]
--- src/app/dodaj-wydatek/dodaj-wydatek.component.ts
+++ src/app/dodaj-wydatek/dodaj-wydatek.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
 import { ListaWydatkowService } from '../lista-wydatkow.service';
 import { Wydatek } from '../wydatek';
 @Component({
@@ -13,7 +14,7 @@ export class DodajWydatekComponent implements OnInit {
  
 kategorie: string[];
   
-constructor(private wydatkiService: ListaWydatkowService) { }
+constructor(private wydatkiService: ListaWydatkowService, private router: Router) { }
 
   ngOnInit() {
         this.nowyWydatek = new Wydatek('Tankowanie', null, null, null);
@@ -22,5 +23,7 @@ constructor(private wydatkiService: ListaWydatkowService) { }
 onSubmit() {
     this.wydatkiService.dodajWydatek(this.nowyWydatek);
     this.nowyWydatek = new Wydatek('Tankowanie', null, null, null);
+    
+    this.router.navigate(['/wydatki']);
   }
 }
